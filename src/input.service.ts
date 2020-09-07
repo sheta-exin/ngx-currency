a8da9cf1e33a16e8e87edca0205b6d516d01a52e
sheta-exin <49760184+sheta-exin@users.noreply.github.com>
2020-09-07 18:54:33 +0200

    Update input.service.ts

--- src/input.service.ts
+++ src/input.service.ts
@@ -186,10 +186,21 @@ export class InputService {
         return parseFloat(value);
     }
 
-    changeToNegative(): void {
-        if (this.options.allowNegative && this.rawValue != "" && this.rawValue.charAt(0) != "-" && this.value != 0) {
+    //changeToNegative(): void {
+        //if (this.options.allowNegative && this.rawValue != "" && this.rawValue.charAt(0) != "-" && this.value != 0) {
             // Apply the mask to ensure the min and max values are enforced.
-            this.rawValue = this.applyMask(false, "-" + this.rawValue);
+        //    this.rawValue = this.applyMask(false, "-" + this.rawValue);
+      //  }
+    //}
+  changeToNegative() {
+        if (this.options.allowNegative && this.rawValue != " " && this.rawValue.charAt(0) != "-" && this.value != 0) {
+            // Apply the mask to ensure the min and max values are enforced.
+            if (this.rawValue == "" ||this.rawValue == "-"){
+                this.rawValue ="-"
+            }else{
+                this.rawValue = this.applyMask(false, "-" + this.rawValue);
+
+            }
         }
     }
 
